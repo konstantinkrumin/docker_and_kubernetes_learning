058111e3f2b6c8e55a82b9d7cfb491aa1fd38dc2
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-10-04 01:21:29 -0400

    set up liveness probe and pull policy

--- kubernetes-project-01/app.js
+++ kubernetes-project-01/app.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.get('/', (req, res) => {
 	res.send(`
-    <h1>Hello from this NodeJS app!!!</h1>
-    <p>This is new</p>
+    <h1>Hello from this NodeJS app!</h1>
+    <p>This is new paragraph</p>
     <p>Try sending a request to /error and see what happens</p>
   `);
 });
