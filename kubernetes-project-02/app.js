a6f5edb7457775674ab6809984fabc2f38b57f22
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-10-10 23:44:28 -0400

    set up emptyDir volume handling and init the new proj

--- kubernetes-project-02/app.js
+++ kubernetes-project-02/app.js
@@ -11,25 +11,29 @@ const filePath = path.join(__dirname, 'story', 'text.txt');
 app.use(bodyParser.json());
 
 app.get('/story', (req, res) => {
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      return res.status(500).json({ message: 'Failed to open file.' });
-    }
-    res.status(200).json({ story: data.toString() });
-  });
+	fs.readFile(filePath, (err, data) => {
+		if (err) {
+			return res.status(500).json({ message: 'Failed to open file.' });
+		}
+		res.status(200).json({ story: data.toString() });
+	});
 });
 
 app.post('/story', (req, res) => {
-  const newText = req.body.text;
-  if (newText.trim().length === 0) {
-    return res.status(422).json({ message: 'Text must not be empty!' });
-  }
-  fs.appendFile(filePath, newText + '\n', (err) => {
-    if (err) {
-      return res.status(500).json({ message: 'Storing the text failed.' });
-    }
-    res.status(201).json({ message: 'Text was stored!' });
-  });
+	const newText = req.body.text;
+	if (newText.trim().length === 0) {
+		return res.status(422).json({ message: 'Text must not be empty!' });
+	}
+	fs.appendFile(filePath, newText + '\n', err => {
+		if (err) {
+			return res.status(500).json({ message: 'Storing the text failed.' });
+		}
+		res.status(201).json({ message: 'Text was stored!' });
+	});
+});
+
+app.get('/error', () => {
+	process.exit(1);
 });
 
 app.listen(3000);
