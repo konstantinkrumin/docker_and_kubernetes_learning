8181edb4a838466bd8d24370a42c8cd2d23a5250
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-10-16 19:10:18 -0400

    added reverse-proxy to nginx file for the frontend

--- kubernetes-project-03/frontend/src/App.js
+++ kubernetes-project-03/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
 	const [tasks, setTasks] = useState([]);
 
 	const fetchTasks = useCallback(function () {
-		fetch('http://192.168.49.2:31153/tasks', {
+		fetch('/api/tasks', {
 			headers: {
 				Authorization: 'Bearer abc'
 			}
@@ -29,7 +29,7 @@ function App() {
 	);
 
 	function addTaskHandler(task) {
-		fetch('http://192.168.49.2:31153/tasks', {
+		fetch('/api/tasks', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
